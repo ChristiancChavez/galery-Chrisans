5ddcf45e8d7eb020fd27196c44d3204a0e1da910
Christian Chavez <v-cchavez@catchpoint.com>
2022-04-13 16:05:42 -0500

    Add calendar

---
+++ src/components/atoms/Calendar.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { TextField } from '@mui/material';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+type calendarTypes = {
+  testId: string,
+  label: string
+}
+
+const  Calendar = ({testId, label}:calendarTypes) => {
+  const [value, setValue] = useState<Date | null>(null);
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <DatePicker
+        aria-label='este es un calendario para asignar fechas'
+        data-testid={testId}
+        label={label}
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
+  );
+}
+
+export default Calendar;
