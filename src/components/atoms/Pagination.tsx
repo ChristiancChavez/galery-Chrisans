3620ca576162e0704d2b4ede3c03464520971032
Christian Chavez <v-cchavez@catchpoint.com>
2022-07-23 16:49:20 -0500

    Add test case to Pagination

--- src/components/atoms/Pagination.tsx
+++ src/components/atoms/Pagination.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Pagination from '@mui/material/Pagination';
 
-const ClientsPagination = () => <Pagination count={10} />
+
+type clientsPaginationTypes = {
+  testId: string,
+  pages: number
+}
+
+const ClientsPagination = ( {testId, pages }: clientsPaginationTypes) => <Pagination data-testid={testId} count={pages} />
 
 export default ClientsPagination;
