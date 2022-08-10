f208c99ad38a80395ba7ecb51bda84b234cf8141
Christian Chavez <v-cchavez@catchpoint.com>
2022-08-10 12:15:41 -0500

    Update ButtonHome.stories.tsx

--- src/stories/Molecules/ButtonHome.stories.tsx
+++ src/stories/Molecules/ButtonHome.stories.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-import ButtonHome  from "src/components/molecules/ButtonHome";
+import ButtonHome from "src/components/molecules/ButtonHome";
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+import PersonSearchIcon from '@mui/icons-material/PersonSearch';
+import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
+import PlaylistAddCheckIcon from '@mui/icons-material/PlaylistAddCheck';
+import PlaylistRemoveIcon from '@mui/icons-material/PlaylistRemove';
 
 export default {
   title: 'Components/molecules/ButtonHome',
@@ -19,5 +23,38 @@ buttonHomeNewClients.args = {
   children: <PersonAddAltIcon color="action" fontSize="large" />
 };
 
+export const buttonHomeCallClients = Template.bind({})
+buttonHomeCallClients.args = {
+  label:'Llamar Clientes', 
+  testIdButton:'searchClientBtn', 
+  text:'Llamar Clientes', 
+  testIdSubtitle:'searchClientSubt', 
+  children: <PhoneInTalkIcon color="action" fontSize="large" />
+};
 
+export const buttonHomeSearchClients = Template.bind({})
+buttonHomeSearchClients.args = {
+  label:'Buscar Clientes', 
+  testIdButton:'searchClientBtn', 
+  text:'Buscar Clientes', 
+  testIdSubtitle:'searchClientSubt', 
+  children: <PersonSearchIcon color="action" fontSize="large" />
+};
 
+export const buttonHomeGoodClients = Template.bind({})
+buttonHomeGoodClients.args = {
+  label:'Buenos Clientes', 
+  testIdButton:'goodClientBtn', 
+  text:'Buenos Clientes', 
+  testIdSubtitle:'goodClientSubt', 
+  children: <PlaylistAddCheckIcon color="action" fontSize="large" />
+};
+
+export const buttonHomeBadClients = Template.bind({})
+buttonHomeBadClients.args = {
+  label:'Malos Clientes', 
+  testIdButton:'badClientBtn', 
+  text:'Malos Clientes', 
+  testIdSubtitle:'badClientSubt', 
+  children: <PlaylistRemoveIcon color="action" fontSize="large" />
+};
