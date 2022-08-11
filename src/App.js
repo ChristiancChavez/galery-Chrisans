da751cc5db613ada20b88604ddf75bab0c1be548
Christian Chavez <christiancchavez10@hotmail.com>
2022-08-11 16:01:42 -0500

    update ActionsButton

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from "react";
 import "./App.css";
-import TableGalery from "./components/atoms/Table.tsx";
+import ActionsButton from "./components/molecules/ActionsButton.tsx";
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+import PersonSearchIcon from '@mui/icons-material/PersonSearch';
+import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
+import PlaylistAddCheckIcon from '@mui/icons-material/PlaylistAddCheck';
+import PlaylistRemoveIcon from '@mui/icons-material/PlaylistRemove';
 
 function App() {
   const items = [
-    { value: "Sagrado corazón", itemId: 5, name: "Sagrado corazón" },
-    { value: "misericordioso", itemId: 3, name: "misericordioso" },
-    { value: "Sagrada familia", itemId: 4, name: "Sagrada familia" },
-    { value: "Guadalupe", itemId: 8, name: "Guadalupe" },
+    { id: 1, label: "Sagrado corazón", testId: '1', icon: <PersonAddAltIcon /> },
+    { id: 2, label: "misericordioso", testId: '2', icon: <PersonSearchIcon /> },
+    { id: 3, label: "Sagrada familia", testId: '3', icon: <PhoneInTalkIcon /> },
+    { id: 4, label: "Guadalupe", testId: '4', icon: <PlaylistAddCheckIcon /> },
+    { id: 5, label: "Guadalupe infantil", testId: '5', icon: <PlaylistRemoveIcon /> },
   ];
   return (
     <div className="App">
       <h2>Christian</h2>
-      <TableGalery />
+      <ActionsButton buttons={items} />
     </div>
   );
 }
---
+++ src/components/molecules/ActionsButton.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ButtonIcon from 'src/components/atoms/IconButton';
+import { Stack, Divider } from '@mui/material';
+
+type ActionsButtonTypes = {
+  buttons: [{ id: string, label: string, testId: string, icon: React.ReactChild }]
+}
+const ActionsButton = ({ buttons }:ActionsButtonTypes ) => {
+  return (
+    <Stack
+      direction="column"
+      divider={<Divider orientation="horizontal" flexItem />}
+      spacing={2}
+    >
+      {buttons.map(button => <ButtonIcon key={button.id} label={button.label} testId={button.testId}>{button.icon}</ButtonIcon> )}
+    </Stack>
+  );
+};
+
+export default ActionsButton;
